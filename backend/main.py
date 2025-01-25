d2d4a29dd1dce4ce7367a33ccb32012fbddea9dd
winshine0326 <shane10340@gmail.com>
2025-01-25 13:11:55 +0900

    feat: gpt api 연결

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
 
 app = FastAPI()
+
+api_key = os.getenv("API_KEY")
+os.environ["OPENAI_API_KEY"] = api_key
+
+client = OpenAI()
+
+response = client.chat.completions.create(
+    model = 'gpt-4o-mini',
+    messages=[
+        {"role":"system","content":"너는 소설작가야. 5줄 이하로 소설을 써주면 돼"},
+        {"role":"user","content":"금융을 배울 수 있도록 금융과 관련된 개념으로 교육용 소설을 써줘"}
+    ]
+)
+
+print(response.choices[0].message.content)
 
 origins = [
     "http://localhost:5173",
