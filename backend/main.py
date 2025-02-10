69506d346b2dd3c18fb375c4cc3810cf56ab296e
winshine0326 <shane10340@gmail.com>
2025-02-10 16:16:54 +0900

    fix: pdf 요약 모듈 pypdf -> langchain으로 변경

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
-from PyPDF2 import PdfReader
 from openai import OpenAI
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import openai
+from langchain.chains.summarize import load_summarize_chain
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,15 +17,6 @@ os.environ["OPENAI_API_KEY"] = api_key
 
 client = OpenAI()
 
-response = client.chat.completions.create(
-    model = 'gpt-4o-mini',
-    messages=[
-        {"role":"system","content":"너는 소설작가야. 5줄 이하로 소설을 써주면 돼"},
-        {"role":"user","content":"금융을 배울 수 있도록 금융과 관련된 개념으로 교육용 소설을 써줘"}
-    ]
-)
-
-print(response.choices[0].message.content)
 
 origins = [
     "http://localhost:5173",
@@ -35,17 +30,7 @@ app.add_middleware(
     allow_headers = ["*"],
 )
 
-reader = PdfReader("test.pdf")
-pages = reader.pages
-text = ""
-
-for page in pages:
-    sub = page.extract_text()
-    text += sub
-
-print(text)
-
 
 @app.get('/')
 def show_novel():
-    return {"message":response}
+    return {"message"}
