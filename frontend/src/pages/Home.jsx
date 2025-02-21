4236259468bf345e7ae721d9b0b29798372f098d
winshine0326 <shane10340@gmail.com>
2025-02-21 20:58:20 +0900

    feat: 홈 nav바 구성

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import '../assets/css/home.css';
 
-const Home = ()=>{
-    return(
-       <>
-       <div className='homeContainer'>
-        <header>
-            <nav>
-                <span>Sodam</span>
-                <a>Login</a>
-                <a>Blog</a>
-                <a>Github</a>
-            </nav>
-        </header>
-        <main className='homeMain'>
-            <div className='homeMainEffect'>
-                <h1>main</h1>
-            </div>
-        </main>
-        <footer>
+const Home = () => {
+    return (
+        <>
+            <div className='homeContainer'>
+                <main className='homeMain'>
+                    <div className='homeMainEffect'>
+                        <nav className='homeNav'>
+                            <div class="homeNavLogo">
+                                <a href="">Sodam</a>
+                            </div>
+                            <ul class="homeNavMenu">
+                                <li><a href="">Home</a></li>
+                                <li><a href="">Create Stories</a></li>
+                                <li>|</li>
+                                <li><a href="">Notion</a></li>
+                                <li><a href="">Github</a></li>
+                            </ul>
+                        </nav>
+                        <h1>main</h1>
+                    </div>
+                </main>
+                <footer>
 
-        </footer>
-       </div>
-       </> 
+                </footer>
+            </div>
+        </>
     )
 }
 
