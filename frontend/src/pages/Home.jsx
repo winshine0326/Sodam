089ea12017329fb18199f840028d5022eb1302bd
winshine0326 <shane10340@gmail.com>
2025-02-21 21:21:51 +0900

    style: nav바 깃허브, 노션 아이콘 이미지 추가

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../assets/css/home.css';
+import notionIcon from '../assets/images/notionIcon.png';
+import githubIcon from '../assets/images/githubIcon.svg';
 
 const Home = () => {
     return (
@@ -15,8 +17,8 @@ const Home = () => {
                                 <li><a href="">Home</a></li>
                                 <li><a href="">Create Stories</a></li>
                                 <li>|</li>
-                                <li><a href="">Notion</a></li>
-                                <li><a href="">Github</a></li>
+                                <a href="https://tested-arrow-f94.notion.site/Sodam-196b7a0a13588054bcb0c7f83d5341da?pvs=4"><li><img src={notionIcon}/>Notion</li></a>
+                                <a href="https://github.com/winshine0326/Sodam"><li><img src={githubIcon}/>Github</li></a>
                             </ul>
                         </nav>
                         <h1>main</h1>
