909bfcc6e54c8387fec0205a5008629b9953714f
winshine0326 <shane10340@gmail.com>
2025-02-21 21:40:37 +0900

    style: nav바에 폰트 추가

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -11,11 +11,11 @@ const Home = () => {
                     <div className='homeMainEffect'>
                         <nav className='homeNav'>
                             <div class="homeNavLogo">
-                                <a href="">Sodam</a>
+                                <a href="">소담</a>
                             </div>
                             <ul class="homeNavMenu">
-                                <li><a href="">Home</a></li>
-                                <li><a href="">Create Stories</a></li>
+                                <li><a href="">홈</a></li>
+                                <li><a href="">이야기 쓰기</a></li>
                                 <li>|</li>
                                 <a href="https://tested-arrow-f94.notion.site/Sodam-196b7a0a13588054bcb0c7f83d5341da?pvs=4"><li><img src={notionIcon}/></li></a>
                                 <a href="https://github.com/winshine0326/Sodam"><li><img src={githubIcon}/></li></a>
