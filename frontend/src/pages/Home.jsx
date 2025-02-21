5894e1328c99593c86e5311cd1a1c964c46ade69
winshine0326 <shane10340@gmail.com>
2025-02-21 22:36:51 +0900

    feat: 이야기 작성하기 버튼 추가

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -26,6 +26,7 @@ const Home = () => {
                             <span>개념 정리용 PDF를 바탕으로 판타지 소설을 생성하는 AI 서비스.<br/>
                                 배움을 이야기로 풀어내어, 지식을 더 쉽게 이해하고 즐길 수 있도록 돕습니다.</span>
                         </article>
+                        <a><button className="homeGoToStoryBtn">이야기 작성하기</button></a>
                     </div>
                 </main>
                 <footer>
