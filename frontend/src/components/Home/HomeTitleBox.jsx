3a09f92c08e7e01de9ac5a6af3eb07f17a401b81
winshine0326 <shane10340@gmail.com>
2025-02-21 23:47:57 +0900

    feat: 홈 타이틀 박스 컴포넌트 분리

---
+++ frontend/src/components/Home/HomeTitleBox.jsx
@@ -0,0 +1,13 @@
+import '../../assets/css/home.css';
+
+const HomeTitleBox = () => {
+    return (
+        <article className='homeTitleBox'>
+            <h1>소설 속에 담긴 지식, <br />배움의 조각이 이야기가 되다 - 소담</h1>
+            <span>개념 정리용 PDF를 바탕으로 판타지 소설을 생성하는 AI 서비스.<br />
+                배움을 이야기로 풀어내어, 지식을 더 쉽게 이해하고 즐길 수 있도록 돕습니다.</span>
+        </article>
+    )
+}
+
+export default HomeTitleBox;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../assets/css/home.css';
 import HomeNav from '../components/Home/HomeNav.jsx';
+import HomeTitleBox from '../components/Home/HomeTitleBox.jsx';
 
 const Home = () => {
     return (
@@ -9,11 +10,7 @@ const Home = () => {
                 <main className='homeMain'>
                     <div className='homeMainEffect'>
                         <HomeNav/>
-                        <article className='homeTitleBox'>
-                            <h1>소설 속에 담긴 지식, <br />배움의 조각이 이야기가 되다 - 소담</h1>
-                            <span>개념 정리용 PDF를 바탕으로 판타지 소설을 생성하는 AI 서비스.<br/>
-                                배움을 이야기로 풀어내어, 지식을 더 쉽게 이해하고 즐길 수 있도록 돕습니다.</span>
-                        </article>
+                        <HomeTitleBox/>
                         <a><button className="homeGoToStoryBtn">이야기 작성하기</button></a>
                     </div>
                 </main>
