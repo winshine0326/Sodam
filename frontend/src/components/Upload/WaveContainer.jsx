6394f8127f05cc1df4c835c30ee22657be48819e
winshine0326 <shane10340@gmail.com>
2025-02-22 20:35:09 +0900

    feat: 선택된 파일 보여주기

--- frontend/src/components/Upload/WaveContainer.jsx
+++ frontend/src/components/Upload/WaveContainer.jsx
@@ -1,15 +1,31 @@
 import '../../assets/css/upload.css';
 import sodamLogo from '../../assets/images/sodamLogo.png';
+import { useState } from 'react';
 
 const WaveContainer = () => {
+    const [file, setFile] = useState();
+
+    const handleFile = (e) => {
+        const selectedFile = e.target.files[0];
+        setFile(selectedFile);
+    }
+
     return (
-        <div className="wave-container">
-            <input type="file" id="file-input" />
-            <label htmlFor="file-input" className="upload-label"><img src={sodamLogo} /></label>
-            <div className="circle"></div>
-            <div className="circle"></div>
-            <div className="circle"></div>
-        </div>
+        <>
+            <div className="wave-container">
+                <input type="file" id="file-input" accept=".pdf" multiple onChange={handleFile} />
+                <label htmlFor="file-input" className="upload-label"><img src={sodamLogo} /></label>
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+                {file && (
+                    <div className='selectedFile'>
+                        <p>선택된 파일: {file.name}</p>
+                        {/* <button onClick={handleSubmit}>제출</button> */}
+                    </div>
+                )}
+            </div>
+        </>
     )
 }
 
