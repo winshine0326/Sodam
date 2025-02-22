405f25fb23822e500e8dfc24db953029ef233889
winshine0326 <shane10340@gmail.com>
2025-02-22 14:10:25 +0900

    style: 트랜지션 세부 조정

--- frontend/src/components/Home/HomeGoToStoryBtn.jsx
+++ frontend/src/components/Home/HomeGoToStoryBtn.jsx
@@ -5,7 +5,7 @@ const HomeGoToStoryBtn = () =>{
     const [visible, setVisible] = useState(false);
 
     useEffect(() => {
-        const timer = setTimeout(() => setVisible(true), 1000);
+        const timer = setTimeout(() => setVisible(true), 1500);
         return () => clearTimeout(timer);
     }, []);
     
@@ -13,7 +13,7 @@ const HomeGoToStoryBtn = () =>{
         <a style={{
             opacity: visible ? 1 : 0,
             transform: visible ? "translateY(0)" : "translateY(20px)",
-            transition: "opacity 1s ease-out, transform 0.6s ease-out",
+            transition: "opacity 0.5s ease-out, transform 0.5s ease-out",
         }}><button className="homeGoToStoryBtn">이야기 작성하기</button></a>
     )
 }
