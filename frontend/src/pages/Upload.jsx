8cb1b6711233401e407bee8005f243eaeac4a5ac
winshine0326 <shane10340@gmail.com>
2025-02-22 17:00:16 +0900

    style: 소담 로고 파일 업로드 버튼에 추가

--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Global/Navbar.jsx';
 import '../assets/css/upload.css';
+import sodamLogo from '../assets/images/sodamLogo.png';
 
 const Upload = () => {
     return (
@@ -8,7 +9,7 @@ const Upload = () => {
             <Navbar />  
             <div className="wave-container">
                 <input type="file" id="file-input" />
-                <label htmlFor="file-input" className="upload-label">파일 선택</label>
+                <label htmlFor="file-input" className="upload-label"><img src={sodamLogo}/></label>
                 <div className="circle"></div>
                 <div className="circle"></div>
                 <div className="circle"></div>
