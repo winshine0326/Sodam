8a2b45caedb792a0945d253e7c887063eb243743
winshine0326 <shane10340@gmail.com>
2025-02-27 22:00:37 +0900

    feat: 로딩 페이지 퍼블리싱

--- frontend/src/components/Reading/Loading.jsx
+++ frontend/src/components/Reading/Loading.jsx
@@ -1,9 +1,14 @@
+import '../../assets/css/reading.css';
+import lodingGirl from '../../assets/images/lodingGirl.png';
 
 const Loading = ()=>{
     return(
         <>
-        <div className="lodingContainer">
-            <h1>hello</h1>
+        <div className="loadingContainer">
+            <div className='loadingMain'>
+                <img src={lodingGirl}/>
+                <h1>열심히 소설을 작성 중입니다....</h1>
+            </div>
         </div>
         </>
     )
--- frontend/src/pages/Reading.jsx
+++ frontend/src/pages/Reading.jsx
@@ -1,4 +1,5 @@
 import Loading from '../components/Reading/Loading';
+import '../assets/css/reading.css';
 
 const Reading = ()=>{
     return(
