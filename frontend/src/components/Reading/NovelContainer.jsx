8bb9ed09173088af4f86126890a0a606564f24d1
winshine0326 <shane10340@gmail.com>
2025-03-01 22:59:09 +0900

    feat: 소설 읽기 페이지 뒤로가기 버튼 추가

--- frontend/src/components/Reading/NovelContainer.jsx
+++ frontend/src/components/Reading/NovelContainer.jsx
@@ -1,14 +1,22 @@
 import '../../assets/css/reading.css';
 
 const NovelContainer = ({ novel }) =>{
+    const goBack = ()=>{
+        history.back();
+    }
+
+
     // console.log(novel);
     const novelText = novel.content;
     const pdfTitle = novel.filename;
     
     return(
         <div className='novelMainContainer'>
-            <h1>{pdfTitle}</h1>
-            <h3 className='novelText'>{novelText}</h3>
+            <div>
+                <h1>{pdfTitle}</h1>
+                <h3 className='novelText'>{novelText}</h3>
+            </div>
+            <button className='backBtn' onClick={goBack}>돌아가기</button>
         </div>
     )
 }
